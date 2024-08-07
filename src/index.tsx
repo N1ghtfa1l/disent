cba64c510e1bb68e3bc6153f78872eac9d3adbe3
N1ghtfa1l <m.pilipchuk2004@mail.ru>
2024-08-07 15:21:43 +0300

    routes

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='https://n1ghtfa1l.github.io/disent/'>
     <App />
   </BrowserRouter>
 );
